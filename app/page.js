a9efd09fce98e0e17163ff2e78cd82b4609e4d17
w-michelle <89858197+w-michelle@users.noreply.github.com>
2023-05-29 21:33:57 -0400

    login page edit

--- app/page.js
+++ app/page.js
@@ -48,20 +48,20 @@ export default function Home() {
     <main>
       {!user && (
         <div className="h-screen flex justify-center items-center">
-          <div className="flex flex-col items-center w-1/2 py-6 rounded-lg shadow-custom">
+          <div className="flex flex-col items-center w-3/4 py-6 rounded-lg shadow-custom">
             <h3 className="text-sm font-medium my-6 text-white">
               cyber bubble
             </h3>
-            <div className="w-3/4">
+            <div className="sm:text-lg w-3/4 text-xs">
               <button
                 onClick={GoogleLogin}
-                className="text-white bg-transparent border-[2px] border-grey py-2 px-4 w-full font-medium rounded-lg flex items-start gap-3 mb-8"
+                className="text-white bg-transparent border-[2px] border-grey py-2 px-4 w-full font-medium rounded-lg flex items-center gap-3 mb-8"
               >
-                <FcGoogle className="text-2xl" /> Sign in with Google
+                <FcGoogle className=" text-2xl" /> Sign in with Google
               </button>
               <button
                 onClick={guestSignIn}
-                className="text-white bg-transparent border-[2px] border-greyBlue py-2 px-4 w-full font-medium rounded-lg flex items-start gap-3 mb-8"
+                className="text-white bg-transparent border-[2px] border-greyBlue py-2 px-4 w-full font-medium rounded-lg flex items-center gap-3 mb-8"
               >
                 <RxPerson className="text-2xl" /> Sign in Anonymously
               </button>
